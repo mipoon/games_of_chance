fee240ae71cf8b0e77499350edcc373eebc9b8be
Michael Poon <107957557+mipoon@users.noreply.github.com>
2024-07-01 01:03:11 +0000

    Remove imports of game classes to clean up namespace

--- games/__init__.py
+++ games/__init__.py
@@ -1,5 +1,1 @@
 from .game_factory import GameFactory
-from .abstract_game import AbstractGame
-from .guess_the_number import GuessTheNumber
-from .heads_or_tails import HeadsOrTails
-from .roll_the_dice import RollTheDice
