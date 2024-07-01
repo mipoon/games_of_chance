addcb8078bfff4933f528f3f0d8908104ec8e523
Michael Poon <107957557+mipoon@users.noreply.github.com>
2024-07-01 01:55:59 +0000

    Clean up GameFactory

--- games/game_factory.py
+++ games/game_factory.py
@@ -1,4 +1,5 @@
-from random import randint
+from random import choice
+
 from .guess_the_number import GuessTheNumber
 from .heads_or_tails import HeadsOrTails
 from .roll_the_dice import RollTheDice
@@ -6,17 +7,11 @@ from .roll_the_dice import RollTheDice
 
 class GameFactory:
 
-    @staticmethod
-    def create_game(game_choice):
-        if game_choice == 1:
-            return GuessTheNumber()
-        if game_choice == 2:
-            return HeadsOrTails()
-        if game_choice == 3:
-            return RollTheDice()
-        raise ValueError("Invalid game type specified.")
+    game_library = [GuessTheNumber, HeadsOrTails, RollTheDice]
+    # # could import AbstractGame and use the line below instead but pylint hates that the concrete class imports don't get used
+    # game_library = [v for k, v in globals().items() if isinstance(v, type) and issubclass(v, AbstractGame) and v is not AbstractGame]
 
     @staticmethod
     def pick_random_game():
-        game_choice = randint(1, 3)
-        return GameFactory.create_game(game_choice)
+        game_choice = choice(GameFactory.game_library)
+        return game_choice()
